237264e255994cb8d49ef8930f1fb8eaaa0247b6
linx <linxiang@clzytech.com>
2025-09-01 15:27:05 +0800

    调整 项目结构

---
+++ framework/zwei-common/src/main/java/com/zwei/common/enums/OperatorType.java
@@ -0,0 +1,24 @@
+package com.zwei.common.enums;
+
+/**
+ * 操作人类别
+ * 
+ * @author zwei
+ */
+public enum OperatorType
+{
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE
+}
