21db1eb6ad22762fc1c49c520142c06dab6226f7
JIARUI ZHOU <502613382@qq.com>
2026-06-05 11:41:15 +0800

    fix: 修复编译错误
    Signed-off-by: JIARUI ZHOU <502613382@qq.com>

--- server/zwei-log/src/test/java/com/zwei/log/task/LogCleanupTaskTest.java
+++ server/zwei-log/src/test/java/com/zwei/log/task/LogCleanupTaskTest.java
@@ -1,12 +1,13 @@
 package com.zwei.log.task;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import com.zwei.common.core.redis.RedisCache;
 import com.zwei.log.infrastructure.config.LogModuleProperties;
 import com.zwei.log.infrastructure.persistence.mysql.AuthLogMapper;
 import com.zwei.log.infrastructure.persistence.mysql.LogStreamCheckpointMapper;
 import com.zwei.log.infrastructure.persistence.mysql.OperationLogMapper;
 import com.zwei.log.infrastructure.persistence.mysql.RuntimeLogMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class LogCleanupTaskTest {
 
@@ -20,7 +21,9 @@ class LogCleanupTaskTest {
         properties.setCleanupRetentionDays(30);
         properties.setCleanupCheckpointRetentionDays(30);
         properties.setCleanupBatchSize(1000);
-        LogCleanupTask task = new LogCleanupTask(operationLogMapper, authLogMapper, runtimeLogMapper, checkpointMapper, properties);
+        RedisCache redisCache = Mockito.mock(RedisCache.class);
+        Mockito.when(redisCache.getCacheObject(Mockito.anyString())).thenReturn(null);
+        LogCleanupTask task = new LogCleanupTask(operationLogMapper, authLogMapper, runtimeLogMapper, checkpointMapper, properties, redisCache);
 
         Mockito.when(operationLogMapper.deleteBefore(Mockito.any(), Mockito.eq(1000))).thenReturn(1000, 1000, 20);
         Mockito.when(authLogMapper.deleteBefore(Mockito.any(), Mockito.eq(1000))).thenReturn(15);
