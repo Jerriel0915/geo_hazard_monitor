868b516ed524b043ffd7b50afb43a17b4c4c111e
JIARUI ZHOU <502613382@qq.com>
2026-06-11 23:58:44 +0800

    chore: update vite.config.tx
    Signed-off-by: JIARUI ZHOU <502613382@qq.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8080',
+                target: 'http://124.221.142.86:80',
                 changeOrigin: true,
             }
         }
